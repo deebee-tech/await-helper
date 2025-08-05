8827bac8a8393507e9ac503717782ae27e2d5bfd
Doug Shontz, Jr. <doug@shontz.net>
2025-08-05 07:36:35 -0400

    feat(retry): Adding executeWithRetry

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,58 @@ export default class AwaitHelper {
     * for async calls that tend to swallow errors and get lost in the
     * promise chain.  This method will throw the error if there is one.
     */
-   // deno-lint-ignore require-await
    public static execute = async <T>(promise: Promise<T>): Promise<T> => {
       return promise
          .then<T>((data: T) => data)
          .catch((error: Error) => {
             throw error;
          });
+   };
+
+   /** Execute a promise with retry logic.  This will retry the promise
+    * up to maxRetries times, waiting interval milliseconds between retries.
+    * If the promise resolves, it returns the resolved value. If it fails
+    * after all retries, it throws the last error.
+    */
+   public static executeWithRetry = async <T>(
+      promise: Promise<T>,
+      currentRetryCount?: number,
+      maxRetries?: number,
+      interval?: number,
+   ): Promise<T> => {
+      if (!currentRetryCount || typeof currentRetryCount !== "number") {
+         currentRetryCount = 0; // Default current retry count
+      }
+
+      if (!maxRetries || typeof maxRetries !== "number") {
+         maxRetries = 3; // Default retry count
+      }
+
+      if (!interval || typeof interval !== "number") {
+         interval = 1000; // Default retry interval
+      }
+
+      let retries = currentRetryCount;
+
+      return promise
+         .then<T>((data: T) => data)
+         .catch(async (error: Error) => {
+            if (retries < maxRetries) {
+               retries++;
+               await AwaitHelper.sleep(interval);
+               return AwaitHelper.executeWithRetry<T>(promise, retries, maxRetries, interval);
+            } else {
+               throw error;
+            }
+         });
+   };
+
+   /** Sleep for a specified number of milliseconds.  This is useful
+    * for delaying execution in async-await code.
+    */
+   private static sleep = async (ms: number) => {
+      return new Promise((resolve) => {
+         setTimeout(resolve, ms);
+      });
    };
 }
--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "vitest";
 import AwaitHelper from "../src";
 
-describe("AwaitHelper", () => {
+describe("AwaitHelper.execute", () => {
    it("should return a promise", () => {
       const result = AwaitHelper.execute<number>(Promise.resolve(1));
       expect(result).toBeInstanceOf(Promise);
@@ -11,6 +11,21 @@ describe("AwaitHelper", () => {
    it("should throw an error", () => {
       const error = new Error("Test Error");
       const result = AwaitHelper.execute<number>(Promise.reject(error));
+      expect(result).toBeInstanceOf(Promise);
+      expect(result).rejects.toThrowError(error);
+   });
+});
+
+describe("AwaitHelper.executeWithRetry", () => {
+   it("should return a promise", () => {
+      const result = AwaitHelper.executeWithRetry<number>(Promise.resolve(1));
+      expect(result).toBeInstanceOf(Promise);
+      expect(result).resolves.toBe(1);
+   });
+
+   it("should throw an error", () => {
+      const error = new Error("Test Error");
+      const result = AwaitHelper.executeWithRetry<number>(Promise.reject(error));
       expect(result).toBeInstanceOf(Promise);
       expect(result).rejects.toThrowError(error);
    });
